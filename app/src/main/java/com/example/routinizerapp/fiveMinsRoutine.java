540b35597f201b68e495447f9ac4f82d516eecad
PrakhyathKS <prakhyath2502@gmail.com>
2023-06-20 18:18:07 +0530

    master of origin

---
+++ app/src/main/java/com/example/routinizerapp/fiveMinsRoutine.java
@@ -0,0 +1,14 @@
+package com.example.routinizerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fiveMinsRoutine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_five_mins_routine);
+    }
+}
