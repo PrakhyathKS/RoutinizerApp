6c6d284256beca0b95bb2bc7953a03e84b7e134b
PrakhyathKS <prakhyath2502@gmail.com>
2023-06-23 20:59:45 +0530

    master of origin

--- app/src/main/java/com/example/routinizerapp/Adapter.java
+++ app/src/main/java/com/example/routinizerapp/Adapter.java
@@ -1,55 +1,55 @@
-package com.example.routinizerapp;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.List;
-
-public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
-
-    private List<ModelClass> remiderlist;
-
-    Adapter(List<ModelClass> remiderlist){
-    this.remiderlist=remiderlist;
-    }
-    @NonNull
-    @Override
-    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-     View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.reminder,parent,false);
-     return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
-    //String rname=remiderlist.get(position).getReminder();
-    //String rcount=remiderlist.get(position).getRemicounter();
-
-    //dholder.setDate(rname,rcount);
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-
-      // TextView ReminderName,Remicount;
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            //ReminderName=ReminderName.findViewById(R.id.ReminderName);
-          //  Remicount=Remicount.findViewById(R.id.Remicount);
-        }
-
-        public void setDate(String rname, String rcount) {
-            //ReminderName.setText(rname);
-          //  Remicount.setText(rcount);
-        }
-    }
-}
-///hfahjsdfhadfklhjafshjasdfhhhhgg
+//package com.example.routinizerapp;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import java.util.List;
+//
+//public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+///*
+//    private List<ModelClass> remiderlist;
+//
+//    Adapter(List<ModelClass> remiderlist){
+//    this.remiderlist=remiderlist;
+//    }
+//    @NonNull
+//    @Override
+//    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//     View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.reminder,parent,false);
+//     return new ViewHolder(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+//    //String rname=remiderlist.get(position).getReminder();
+//    //String rcount=remiderlist.get(position).getRemicounter();
+//
+//    //dholder.setDate(rname,rcount);
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//
+//      // TextView ReminderName,Remicount;
+//        public ViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            //ReminderName=ReminderName.findViewById(R.id.ReminderName);
+//          //  Remicount=Remicount.findViewById(R.id.Remicount);
+//        }
+//
+//        public void setDate(String rname, String rcount) {
+//            //ReminderName.setText(rname);
+//          //  Remicount.setText(rcount);
+//        }
+//    }
+//}*/
+/////hfahjsdfhadfklhjafshjasdfhhhhgg
